d13acce462f0825a8f765241434c9a5c60680641
dxo <hi@d-xo.org>
2024-07-10 17:29:31 +0200

    flake: install hm-typecheck

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     solcore.url = "github:d-xo/solcore?ref=alex-happy-cabal";
+    hm-typecheck.url = "github:mbenke/hm-typecheck";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -14,6 +15,7 @@
       devShells.default = pkgs.mkShell {
         buildInputs = [
           (pkgs.haskell.lib.dontCheck ins.solcore.packages.${system}.default)
+          ins.hm-typecheck.packages.${system}.default
         ];
       };
     });
